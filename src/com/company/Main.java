7bd7bfbc0bb9da40f2ffeb0bbde715e34ee6300f
BuildTools <unconfigured@null.spigotmc.org>
2019-12-05 16:16:02 +0100

    Gestion du menu dans la classe Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,89 @@
 package com.company;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        GestionProgrammationSemaine gestionProgrammationSemaine = new GestionProgrammationSemaine();
+
+        // Gestion du menu
+        try {
+            Scanner scanner = new Scanner(System.in);
+            boolean running = true;
+            do {
+                afficherMenu();
+                System.out.print("Faites votre choix: ");
+                if (scanner.hasNextInt()) {
+
+                    int choice = scanner.nextInt();
+                    if (choice < 0 || choice > 9) {
+                        System.out.println("le choix doit etre entre 0 et 9");
+                        continue;
+                    } else {
+                        switch (choice) {
+                            case 0: // Reinitialiser programmation
+                                System.out.println("CHOIX 0");
+                                break;
+                            case 1: // Ajouter Film
+                                System.out.println("CHOIX 1");
+                                break;
+                            case 2: // Ajouter piece de theatre
+                                System.out.println("CHOIX 2");
+                                break;
+                            case 3: // Ajouter un nom d'inteprete a un spectacle
+                                System.out.println("CHOIX 3");
+                                break;
+                            case 4: // Ajouter une seance pour un film
+                                System.out.println("CHOIX 4");
+                                break;
+                            case 5: // Ajouter une seance pour une piece de theatre
+                                System.out.println("CHOIX 5");
+                                break;
+                            case 6: // Vendre des places pour un film
+                                System.out.println("CHOIX 6");
+                                break;
+                            case 7: // Vendre des places pour une pieces de theatre
+                                System.out.println("CHOIX 7");
+                                break;
+                            case 8: // Consulter chiffre d'affaire et taux de remplissage
+                                System.out.println("CHOIX 8");
+                                break;
+                            case 9:
+                                running = false;
+                                break;
+                        }
+                    }
+                } else {
+                    System.out.println("Votre choix doit etre un entier");
+                    scanner.next();
+                    continue;
+                }
+
+            } while (running);
+            System.out.println("Arret en cours...");
+        } catch (Exception e)
+        {
+            System.err.println(e);
+        }
+
     }
+
+    private static void afficherMenu()
+    {
+        System.out.println("reinitialiser programmation (0)");
+        System.out.println("ajouter un film (1)");
+        System.out.println("ajouter une piece de theatre (2)");
+        System.out.println("ajouter un nom d'interprete a un spectacle (3)");
+        System.out.println("ajouter une seance pour un film (4)");
+        System.out.println("ajouter une seance pour une piece de theatre (5)");
+        System.out.println("vendre des places pour un film (6)");
+        System.out.println("vendre des places pour une piece de theatre (7)");
+        System.out.println("consulter le chiffre d'affaire et le taux de remplissage d'un spectacle (8)");
+        System.out.println("quitter (9)");
+    }
+
 }
--- src/com/company/interfaces/IProgrammationSemaine.java
+++ src/com/company/interfaces/IProgrammationSemaine.java
@@ -1,4 +1,4 @@
-Ôªøpackage com.company.interfaces;
+package com.company.interfaces;
 
 import com.company.Horaire;
 import com.company.spectacles.Film;
@@ -10,89 +10,89 @@ import java.util.List;
 
 public interface IProgrammationSemaine {
     /**
-     * Recherche un film existant ayant pour titre et pour r√©alisateur ceux pass√©s en param√®tre
+     * Recherche un film existant ayant pour titre et pour rÈalisateur ceux passÈs en paramËtre
      * @param titre
      * @param realisateur
-     * @return le film trouv√© ou null si aucun film trouv√©
+     * @return le film trouvÈ ou null si aucun film trouvÈ
      */
     public Film rechercherFilm (String titre, String realisateur);
 
     /**
-     * Recherche un film ayant pour titre et pour r√©alisateur ceux pass√©s en param√®tre.
-     * Si aucun film trouv√©, cr√©e le film et l'ajoute sinon l√®ve une exception
+     * Recherche un film ayant pour titre et pour rÈalisateur ceux passÈs en paramËtre.
+     * Si aucun film trouvÈ, crÈe le film et l'ajoute sinon lËve une exception
      * @param titre
      * @param realisateur
      * @param duree
-     * @throws IllegalArgumentException Le film existe d√©j√†
+     * @throws IllegalArgumentException Le film existe dÈj‡
      */
     public void ajouterFilm (String titre, String realisateur, int duree);
 
     /**
-     * Ajoute l'interpr√®te pass√© en param√®tre au spectacle correspondant au param√®tre numSpectacle s'il existe sinon l√®ve une exception
+     * Ajoute l'interprËte passÈ en paramËtre au spectacle correspondant au paramËtre numSpectacle s'il existe sinon lËve une exception
      * @param numSpectacle
      * @param interprete
      * @throws IllegalArgumentException Spectacle inexistant
      */
     public void ajouterInterprete( int numSpectacle, String interprete);
     /**
-     * Recherche une pi√®ce de th√©√¢tre existante ayant pour titre et pour metteur en sc√®ne ceux pass√©s en param√®tre
+     * Recherche une piËce de thÈ‚tre existante ayant pour titre et pour metteur en scËne ceux passÈs en paramËtre
      * @param titre
      * @param metteurEnScene
-     * @return la pi√®ce de th√©√¢tre trouv√©e ou null si aucune pi√®ce trouv√©e
+     * @return la piËce de thÈ‚tre trouvÈe ou null si aucune piËce trouvÈe
      */
     public PieceTheatre rechercherPiece (String titre, String metteurEnScene);
     /**
-     * Recherche une pi√®ce de th√©√¢tre ayant pour titre et pour metteur en sc√®ne ceux pass√©s en param√®tre.
-     * Si aucune pi√®ce trouv√©e, cr√©e la pi√®ce et l'ajoute sinon l√®ve une exception
+     * Recherche une piËce de thÈ‚tre ayant pour titre et pour metteur en scËne ceux passÈs en paramËtre.
+     * Si aucune piËce trouvÈe, crÈe la piËce et l'ajoute sinon lËve une exception
      * @param titre
      * @param metteurEnScene
      * @param nbEntractes
-     * @throws IllegalArgumentException La pi√®ce existe d√©j√†
+     * @throws IllegalArgumentException La piËce existe dÈj‡
      */
     public void ajouterPiece (String titre, String metteurEnScene, int nbEntractes);
 
     /**
-     * Cr√©e et ajoute la s√©ance au film correspondant √† idFilm s'il existe
-     * et si la salle est disponible sur le cr√©neau d√©fini par les param√®tres jour et d√©but et la dur√©e du film.
-     * Il faut que l'heure de l'horaire de fin calcul√©e soit compris entre 0 et 23 et les minutes entre 0 et 59.
-     * L√®ve une exception si aucune salle ne correspond ) idSalle
-     * Ajoute √©galement le cr√©neau √† la salle correspondante
+     * CrÈe et ajoute la sÈance au film correspondant ‡ idFilm s'il existe
+     * et si la salle est disponible sur le crÈneau dÈfini par les paramËtres jour et dÈbut et la durÈe du film.
+     * Il faut que l'heure de l'horaire de fin calculÈe soit compris entre 0 et 23 et les minutes entre 0 et 59.
+     * LËve une exception si aucune salle ne correspond ) idSalle
+     * Ajoute Ègalement le crÈneau ‡ la salle correspondante
      * @param idFilm
      * @param jour
      * @param debut
      * @param idSalle
      * @throws IllegalArgumentException Film inexistant
      * @throws IllegalArgumentException Salle inexistante
-     * @throws IllegalStateException Cr√©neau indisponible pour dans cette salle
+     * @throws IllegalStateException CrÈneau indisponible pour dans cette salle
      */
     public void ajouterSeanceFilm(int idFilm, int jour, Horaire debut, int idSalle);
 
     /**
-     * Teste l'existence d'une s√©ance pour la pi√®ce de th√©√¢tre correspondant √† idPiece si elle existe.
+     * Teste l'existence d'une sÈance pour la piËce de thÈ‚tre correspondant ‡ idPiece si elle existe.
      * @param idPiece
      * @param jour
      * @return
-     * @throws IllegalArgumentException Pi√®ce inexistante
+     * @throws IllegalArgumentException PiËce inexistante
      */
     public boolean existeSeanceCeJour(int idPiece, int jour);
     /**
-     * Cr√©e et ajoute la s√©ance √† la pi√®ce correspondant √† idPiece s'il existe
-     * et s'il n'y a pas d√©j√† un cr√©neau d√©fini pour cette salle ce jour l√†.
-     * Pour toute les pi√®ces, on d√©finira un cr√©neau d'une dur√©e de 3h. Si en ajoutant 3 heures √† l'horaire de d√©but, on passe au jour suivant (h>=24) il faut ramener l'heure entre 0 et 23.
-     * L√®ve une exception si aucune salle ne correspond √† idSalle
-     * Ajoute √©galement le cr√©neau √† la salle
+     * CrÈe et ajoute la sÈance ‡ la piËce correspondant ‡ idPiece s'il existe
+     * et s'il n'y a pas dÈj‡ un crÈneau dÈfini pour cette salle ce jour l‡.
+     * Pour toute les piËces, on dÈfinira un crÈneau d'une durÈe de 3h. Si en ajoutant 3 heures ‡ l'horaire de dÈbut, on passe au jour suivant (h>=24) il faut ramener l'heure entre 0 et 23.
+     * LËve une exception si aucune salle ne correspond ‡ idSalle
+     * Ajoute Ègalement le crÈneau ‡ la salle
      * @param idPiece
      * @param jour
      * @param debut
      * @param idSalle
-     * @throws IllegalArgumentException Pi√®ce inexistante
+     * @throws IllegalArgumentException PiËce inexistante
      * @throws IllegalArgumentException Salle inexistante
-     * @throws IllegalStateException Cr√©neau indisponible pour dans cette salle
+     * @throws IllegalStateException CrÈneau indisponible pour dans cette salle
      */
     public void ajouterSeanceTheatre(int idPiece, int jour, Horaire debut, int idSalle);
 
     /**
-     * Retourne le chiffre d'affaires du spectacle correspondant au num√©ro pass√© en param√®tre s'il existe
+     * Retourne le chiffre d'affaires du spectacle correspondant au numÈro passÈ en paramËtre s'il existe
      * @param numSpectacle
      * @return
      * @throws IllegalArgumentException Spectacle inexistant
@@ -100,7 +100,7 @@ public interface IProgrammationSemaine {
     public double chiffreAffaires(int numSpectacle);
 
     /**
-     * Retourne le taux de remplissage du spectacle correspondant au num√©ro pass√© en param√®tre s'il existe
+     * Retourne le taux de remplissage du spectacle correspondant au numÈro passÈ en paramËtre s'il existe
      * @param numSpectacle
      * @return
      * @throws IllegalArgumentException Spectacle inexistant
@@ -109,85 +109,85 @@ public interface IProgrammationSemaine {
 
 
     /**
-     * Vend le nombre de places √† tarif normal pass√© en param√®tre pour le film correspondant √† idFilm s'il existe
-     * et pour la s√©ance correspondant au jour et √† l'horaire de d√©but pass√©s en param√®tre √† condition qu'il y ait assez de places disponibles
+     * Vend le nombre de places ‡ tarif normal passÈ en paramËtre pour le film correspondant ‡ idFilm s'il existe
+     * et pour la sÈance correspondant au jour et ‡ l'horaire de dÈbut passÈs en paramËtre ‡ condition qu'il y ait assez de places disponibles
      * @param idFilm
      * @param jour
      * @param debut
      * @param nbPlacesTN
      * @throws IllegalArgumentException Film inexistant
-     * @throws IllegalArgumentException S√©ance inexistant
+     * @throws IllegalArgumentException SÈance inexistant
      * @throws IllegalArgumentException Pas assez de places disponibles
      */
 
     public void vendrePlaceFilmTN(int idFilm, int jour, Horaire debut, int nbPlacesTN) ;
     /**
-     * Vend le nombre de places √† tarif r√©duit pass√© en param√®tre pour le film correspondant √† idFilm s'il existe
-     * et pour la s√©ance correspondant au jour et √† l'horaire de d√©but pass√©s en param√®tre √† condition qu'il y ait assez de places disponibles
+     * Vend le nombre de places ‡ tarif rÈduit passÈ en paramËtre pour le film correspondant ‡ idFilm s'il existe
+     * et pour la sÈance correspondant au jour et ‡ l'horaire de dÈbut passÈs en paramËtre ‡ condition qu'il y ait assez de places disponibles
      * @param idFilm
      * @param jour
      * @param debut
      * @param nbPlacesTR
      * @throws IllegalArgumentException Film inexistant
-     * @throws IllegalArgumentException S√©ance inexistant
+     * @throws IllegalArgumentException SÈance inexistant
      * @throws IllegalArgumentException Pas assez de places disponibles
      */
     public void vendrePlaceFilmTR(int idFilm, int jour, Horaire debut, int nbPlacesTR);
     /**
-     * Vend le nombre de places √† tarif normal pass√© en param√®tre pour la pi√®ce correspondant √† idPiece s'elle existe
-     * et pour la s√©ance correspondant au jour pass√© en param√®tre √† condition qu'il y ait assez de places disponibles
+     * Vend le nombre de places ‡ tarif normal passÈ en paramËtre pour la piËce correspondant ‡ idPiece s'elle existe
+     * et pour la sÈance correspondant au jour passÈ en paramËtre ‡ condition qu'il y ait assez de places disponibles
      * @param idPiece
      * @param jour
      * @param nbPlacesTN
      * @throws IllegalArgumentException Film inexistant
-     * @throws IllegalArgumentException S√©ance inexistant
+     * @throws IllegalArgumentException SÈance inexistant
      * @throws IllegalArgumentException Pas assez de places disponibles
      */
     public void vendrePlacePieceTN(int idPiece, int jour, int nbPlacesTN);
     /**
-     * Vend le nombre de places √† tarif fauteuils pass√© en param√®tre pour la pi√®ce correspondant √† idPiece s'elle existe
-     * et pour la s√©ance correspondant au jour pass√© en param√®tre √† condition qu'il y ait assez de places disponibles
+     * Vend le nombre de places ‡ tarif fauteuils passÈ en paramËtre pour la piËce correspondant ‡ idPiece s'elle existe
+     * et pour la sÈance correspondant au jour passÈ en paramËtre ‡ condition qu'il y ait assez de places disponibles
      * @param idPiece
      * @param jour
      * @param nbFauteuils
      * @throws IllegalArgumentException Film inexistant
-     * @throws IllegalArgumentException S√©ance inexistant
+     * @throws IllegalArgumentException SÈance inexistant
      * @throws IllegalArgumentException Pas assez de places disponibles
      */
     public void vendrePlaceFauteuilPiece(int idPiece, int jour, int nbFauteuils);
     /**
      *
-     * @return les films sous forme d'une cha√Æne de caract√®res
+     * @return les films sous forme d'une chaÓne de caractËres
      */
     public String lesFilms ();
     /**
      *
-     * @return les pi√®ces de th√©√¢tre sous forme d'une cha√Æne de caract√®res
+     * @return les piËces de thÈ‚tre sous forme d'une chaÓne de caractËres
      */
     public String lesPieces();
     /**
      *
-     * @return les salles de film sous forme d'une cha√Æne de caract√®res
+     * @return les salles de film sous forme d'une chaÓne de caractËres
      */
     public String lesSallesFilm();
 
     /**
      *
-     * @return les salles de th√©√¢tre sous forme d'une cha√Æne de caract√®res
+     * @return les salles de thÈ‚tre sous forme d'une chaÓne de caractËres
      */
     public String lesSallesTheatre();
 
 
     /**
-     * retourne les s√©ances de la pi√®ce de th√©√¢tre correspondant √† idPiece si elle existe sinon l√®ve une exception
+     * retourne les sÈances de la piËce de thÈ‚tre correspondant ‡ idPiece si elle existe sinon lËve une exception
      * @param idPiece
      * @return
-     * @throws IllegalArgumentException Pi√®ce inexistante
+     * @throws IllegalArgumentException PiËce inexistante
      */
 
     public String lesSeancesTheatre (int idPiece);
     /**
-     * retourne les s√©ances du film correspondant √† idFilm s'il existe sinon l√®ve une exception
+     * retourne les sÈances du film correspondant ‡ idFilm s'il existe sinon lËve une exception
      * @param idFilm
      * @return
      * @throws IllegalArgumentException Film inexistant
@@ -195,53 +195,53 @@ public interface IProgrammationSemaine {
 
     public String lesSeancesFilm (int idFilm);
     /**
-     * Retourne le nombre de places standard disponibles pour le spectacle correspondant au num√©ro pass√© en param√®tre s'il existe
-     * et √† la s√©ance correspondant au jour et √† l'horaire de d√©but pass√©s en param√®tre si elle existe
+     * Retourne le nombre de places standard disponibles pour le spectacle correspondant au numÈro passÈ en paramËtre s'il existe
+     * et ‡ la sÈance correspondant au jour et ‡ l'horaire de dÈbut passÈs en paramËtre si elle existe
      * @param numSpectacle
      * @param jour
      * @param heures
      * @param minutes
      * @return
      * @throws IllegalArgumentException Spectacle inexistant
-     * @throws IllegalArgumentException S√©ance inexistante pour ce spectacle
+     * @throws IllegalArgumentException SÈance inexistante pour ce spectacle
      */
     public int getNbPlacesDispo(int numSpectacle, int jour, int heures, int minutes);
     /**
      * @param idFilm
-     * @return true si idFilm correspond √† un film et false sinon
+     * @return true si idFilm correspond ‡ un film et false sinon
      */
     public boolean existeFilm (int idFilm);
 
     /**
      * @param idPiece
-     * @return true si idPi√®ce correspond √† une pi√®ce de th√©√¢tre et false sinon
+     * @return true si idPiËce correspond ‡ une piËce de thÈ‚tre et false sinon
      */
     public boolean existePiece (int idPiece);
 
     /**
-     * Teste l'existance d'une s√©ance pour le film dont l'idFilm est pass√© en param√®tre s'il existe. Sinon l√®ve une exception
+     * Teste l'existance d'une sÈance pour le film dont l'idFilm est passÈ en paramËtre s'il existe. Sinon lËve une exception
      * @param idFilm
      * @param jour
      * @param heures
      * @param minutes
-     * @return true s'il existe une s√©ance correspondant √† un cr√©neau d√©fini par un jour et un horaire de d√©but donn√© par heures et minutes et false sinon
+     * @return true s'il existe une sÈance correspondant ‡ un crÈneau dÈfini par un jour et un horaire de dÈbut donnÈ par heures et minutes et false sinon
      * @throws IllegalArgumentException Film inexistant
      */
     public boolean existeSeanceFilm (int idFilm, int jour, int heures, int minutes );
 
     /**
      * @param idSalle
-     * @return true si idSalle correspond √† une salle de film et false sinon
+     * @return true si idSalle correspond ‡ une salle de film et false sinon
      */
     public boolean existeSalleFilm (int idSalle);
     /**
      * @param idSalle
-     * @return true si idSalle correspond √† une salle de film et false sinon
+     * @return true si idSalle correspond ‡ une salle de film et false sinon
      */
     public boolean existeSalleTheatre (int idSalle);
 
     /**
-     * Retourne la dur√©e du film correspondant au param√®tre s'il existe
+     * Retourne la durÈe du film correspondant au paramËtre s'il existe
      * @param idFilm
      * @return
      * @throws IllegalArgumentException Film inexistant
@@ -249,58 +249,58 @@ public interface IProgrammationSemaine {
     public int dureeFilm(int idFilm);
 
     /**
-     * Teste la disponibilit√© de la salle dont l'idSalle est pass√© en param√®tre si elle existe
+     * Teste la disponibilitÈ de la salle dont l'idSalle est passÈ en paramËtre si elle existe
      * @param jour
      * @param debut
      * @param duree
      * @param idSalle
-     * @return Retourne true si la salle dont l'idSalle est pass√© en param√®tre est disponible au cr√©neau pass√© en param√®tre sinon retourne false
+     * @return Retourne true si la salle dont l'idSalle est passÈ en paramËtre est disponible au crÈneau passÈ en paramËtre sinon retourne false
      * @throws IllegalArgumentException Salle inexistante
      */
     public boolean salleStandardDisponible (int jour, Horaire debut, int duree, int idSalle);
 
     /**
-     * Supprime les films et les pi√®ces de th√©√¢tre de la programmation en cours.
-     * Il faut √©galement supprimer les cr√©neaux occup√©s de chaque salle
+     * Supprime les films et les piËces de thÈ‚tre de la programmation en cours.
+     * Il faut Ègalement supprimer les crÈneaux occupÈs de chaque salle
      */
     public void reinitialiserProgrammation();
 
     /**
-     * Retourne le nombre de places de type fauteuil disponibles pour la pi√®ce correspondant √† idPiece s'elle existe
-     * et s'il existe une s√©ance le jour pass√© en param√®tre
+     * Retourne le nombre de places de type fauteuil disponibles pour la piËce correspondant ‡ idPiece s'elle existe
+     * et s'il existe une sÈance le jour passÈ en paramËtre
      * @param idPiece
      * @param jour
      * @return
-     * @throws IllegalArgumentException Pi√®ce inexistante
-     * @throws IllegalArgumentException Aucune s√©ance ce jour;
+     * @throws IllegalArgumentException PiËce inexistante
+     * @throws IllegalArgumentException Aucune sÈance ce jour;
      */
     public int getNbFauteuilsDispo(int idPiece, int jour);
 
     /**
-     * Retourne le nombre de places standard disponibles pour la pi√®ce correspondant √† idPiece s'elle existe
-     * et s'il existe une s√©ance le jour pass√© en param√®tre
+     * Retourne le nombre de places standard disponibles pour la piËce correspondant ‡ idPiece s'elle existe
+     * et s'il existe une sÈance le jour passÈ en paramËtre
      * @param idPiece
      * @param jour
      * @return
-     * @throws IllegalArgumentException Pi√®ce inexistante
-     * @throws IllegalArgumentException Aucune s√©ance ce jour;
+     * @throws IllegalArgumentException PiËce inexistante
+     * @throws IllegalArgumentException Aucune sÈance ce jour;
      */
     public int getNbPlacesDispo(int idPiece, int jour);
 
     /**
      *
      * @param numSpectacle
-     * @return true si le num√©ro du spectacle pass√© en param√®tre correspond √† un num√©ro de film et false sinon
+     * @return true si le numÈro du spectacle passÈ en paramËtre correspond ‡ un numÈro de film et false sinon
      */
     public boolean estUnFilm(int numSpectacle);
     /**
      *
      * @param numSpectacle
-     * @return true si le num√©ro du spectacle pass√© en param√®tre correspond √† un num√©ro de pi√®ce de th√©√¢tre et false sinon
+     * @return true si le numÈro du spectacle passÈ en paramËtre correspond ‡ un numÈro de piËce de thÈ‚tre et false sinon
      */
     public boolean estUnePiece(int numSpectacle);
     /**
-     * Retourne le spectacle correspondant au num√©ro pass√© en param√®tre s'il existe et null sinon
+     * Retourne le spectacle correspondant au numÈro passÈ en paramËtre s'il existe et null sinon
      * @param numSpectacle
      * @return
      */
