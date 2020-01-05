6187728914152c2b1043ba6a166e3a209d65fac7
BuildTools <unconfigured@null.spigotmc.org>
2020-01-05 17:40:35 +0100

    javadoc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -115,6 +115,9 @@ public class Main {
         logMessage("Ajout Seance Film ... ");
     }
 
+    /**
+     * @throws IllegalArgumentException une seance est deja programme au meme jour que l'utilisateur a saisie
+     */
     private static void ajouterSeancePiece()
     {
         System.out.println(gestion.lesPieces());
@@ -211,6 +214,12 @@ public class Main {
         System.out.println("Chiffre D'affaire : " + gestion.chiffreAffaires(idSpectacle) + " euro(s) ");
     }
 
+    /**
+     *
+     * @param choice
+     * @returns retourne faux si l'utilisateur decide de quitter
+     * @throws IllegalArgumentException choix n'est pas entre 0 et 9
+     */
     private static boolean gestionMenu(int choice)
     {
         if(choice < 0 || choice > 9)
@@ -239,6 +248,12 @@ public class Main {
         return true;
     }
 
+    /**
+     *
+     * @param message
+     * @return retourne la valeur saisie par l'utilisateur
+     * @throws IllegalArgumentException la valeur saisie n'est pas un entier
+     */
     private static Integer inputValue(String message)
     {
         System.out.print(message);
